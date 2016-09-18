e11985c5de9533f8576aa3dbca1944edfe2d5f6d
yejunqin <yejunqin@qipeipu.com>
2016-09-19 00:11:12 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,32 @@
 var gulp = require('gulp')
 var less = require('gulp-less')
+var connect = require('gulp-connect')
 
+var lessSrc = './less/*.less'
+var lessDist = './css'
+var htmlSrc = './*.html'
+//编译less
 gulp.task('less', function(){
-	gulp.src('./less/*.less')
+	gulp.src(lessSrc)
 		.pipe(less())
-		.pipe(gulp.dest('./css'))
+		.pipe(gulp.dest(lessDist))
+		.pipe(connect.reload())
 })
-
-gulp.task('default', function(){
-	gulp.run('less')
-
-	gulp.watch('./less/*.less', function(){
-		gulp.run('less')
+//html刷新
+gulp.task('html', function(){
+	gulp.src(htmlSrc)
+			.pipe(connect.reload())
+})
+//本地服务器
+gulp.task('connect', function(){
+	connect.server({
+		livereload: true
 	})
 })
+//监听任务
+gulp.task('watch', function(){
+	gulp.watch(lessSrc, ['less'])
+	gulp.watch(htmlSrc, ['html'])
+})
+//默认任务
+gulp.task('default', ['less', 'html', 'watch', 'connect'])
