c5aa1d5eafce8bf2ff674e85e126db69911decf6
叶俊钦 <yejunqin.qipeipu.com>
2016-09-18 18:05:37 +0800

    加入项目基础文件

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp')
+var less = require('gulp-less')
+
+gulp.task('less', function(){
+	gulp.src('./less/*.less')
+		.pipe(less())
+		.pipe(gulp.dest('./css'))
+})
+
+gulp.task('default', function(){
+	gulp.run('less')
+
+	gulp.watch('./less/*.less', function(){
+		gulp.run('less')
+	})
+})
