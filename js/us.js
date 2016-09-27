bdbadf518ee27a247c7a8e07461696fd8e81e420
叶俊钦 <yejunqin.qipeipu.com>
2016-09-27 18:47:31 +0800

    update

--- js/us.js
+++ js/us.js
@@ -1,12 +1,23 @@
 /**
  * Created by  on 2016/9/25.
  */
+function activateItem(idx, items){
+  items.each(function(i, ele){
+    $(ele).removeClass('active')
+  })
+  items.eq(idx).addClass('active')
+}
 $(document).ready(function(){
+  var timeline = $('#timeline')
+  var items = timeline.find('#dates .dates-item')
   var development = new Swiper('#fazhan .swiper-container', {
-    loop: true,
-    //autoplay: 3000,
-    //pagination: '#banner .swiper-pagination',
-    paginationClickable :true
+    // loop: true,
+    paginationClickable :true,
+    nextButton: '#fazhan #next',
+    prevButton: '#fazhan #prev',
+    onSlideChangeStart: function(swiper){
+      activateItem(swiper.activeIndex, items)
+    }
   })
   var paperSlidesPerView = 1
   if(window.screen.width > 760){
@@ -18,7 +29,8 @@ $(document).ready(function(){
     prevButton: '#dongtai .swiper-button .prev',
     spaceBetween: 16
   })
-  // $().timelinr({
-  //   arrowKeys: 'true'
-  // })
+  timeline.on('click', '.dates-item', function(){
+    var _this = $(this)
+    development.slideTo(_this.index())
+  })
 })
